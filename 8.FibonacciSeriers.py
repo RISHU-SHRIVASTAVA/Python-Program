a99b40d876874d73faf0cb99514539a9f2f93f40
Rishu Shrivastava <shrivastava.rishu5@gmail.com>
2021-06-21 18:56:22 +0530

    FibonacciSeries

---
+++ 8.FibonacciSeriers.py
@@ -0,0 +1,20 @@
+x,y=0,1
+temp=0
+n=int(input("Enter lenght of Fibonacci series :"))
+
+if n==1:
+    print(x,end=' ')
+
+elif n==2:
+    print(x, y, end=' ')
+
+elif n>2:
+    print(x, y,end=' ')
+    while(n>2):
+         temp=x+y
+         print(temp,end=' ')
+         x,y=y,temp
+         n-=1
+
+else:
+    print("Please enter number greater than 1 ")
